679f4186476a92ceae96d228f80cd92fa7bb58a7
DolceTseng <jessica.turn613@gmail.com>
2025-02-27 17:21:32 +0800

    update home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,12 @@
 const Home = () => {
-  return <h1>首頁</h1>;
+  return (
+    <div className="container">
+      <h1>首頁</h1>
+      <img
+        src="https://images.unsplash.com/photo-1461727885569-b2ddec0c4328?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8amFwYW5lc2V8ZW58MHx8MHx8fDI%3D"
+        className="img-fluid border rounded"
+      />
+    </div>
+  );
 };
 export default Home;
